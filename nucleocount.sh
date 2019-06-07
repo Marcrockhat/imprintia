b40db724da71b9cea95093b27f8340a4baa41c56
Marcrockhat <marcelinusrocky@aol.com>
2019-06-07 06:50:33 +0200

    Update nucleocount.sh
    Fixing java igvtools command to be compatible with the latest igvtools.

--- nucleocount.sh
+++ nucleocount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TEST=$(cat "$1")
 for i in $TEST; do
-	java -Xmx1024m  -Djava.awt.headless=true -jar ~/IGVTools/igvtools.jar count -w 1 --bases --query $i $2 count.wig $3
+	$5 count -w 1 --bases --query $i $2 count.wig $3
 	BASES=$(sed -n '4p' count.wig)
 	OUTTEXT=$i$'\t'$BASES
 	echo $OUTTEXT >> $4
